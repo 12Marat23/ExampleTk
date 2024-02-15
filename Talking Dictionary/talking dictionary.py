e3702b65d380745253d6ee9d9d6d508674e237fa
12Marat23 <55796725+12Marat23@users.noreply.github.com>
2024-02-15 19:26:55 +0300

    Начало приложения

---
+++ Talking Dictionary/talking dictionary.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("1000x626+150+50")
+root.title("Talking Dictionary")
+root.resizable(False, False)
+
+if __name__ == '__main__':
+    root.mainloop()
